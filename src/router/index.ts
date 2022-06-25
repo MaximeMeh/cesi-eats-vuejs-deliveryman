02c222fb6baf368b08d5ded43813dee4cf06a343
MaximeMeh <44493799+MaximeMeh@users.noreply.github.com>
2022-06-25 18:41:21 +0200

    commit init

---
+++ src/router/index.ts
@@ -0,0 +1,29 @@
+import { createRouter, createWebHashHistory, RouteRecordRaw } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import LoginView from '../views/LoginView.vue'
+import ProfileView from '../views/ProfileView.vue'
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/LoginView.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/ProfileView.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>Vos plats préférés, directement à votre porte</h1>
+  </div>
+  <button>Trouver un restaurant</button>
+</template>
+
+
+
+<style scoped>
+  h1 {
+    text-align: left;
+    margin:32px;
+    line-height: 64px;
+  }
+</style>>
